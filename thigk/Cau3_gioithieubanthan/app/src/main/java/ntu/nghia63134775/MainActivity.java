8ebbcf439435c21a8cbc46140a739683f7f0bf2b
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2024-03-25 11:05:30 +0700

    hoanthanhbai3

--- thigk/Cau3_gioithieubanthan/app/src/main/java/ntu/nghia63134775/MainActivity.java
+++ thigk/Cau3_gioithieubanthan/app/src/main/java/ntu/nghia63134775/MainActivity.java
@@ -16,13 +16,16 @@ public class MainActivity extends AppCompatActivity {
         TextView text = findViewById(R.id.edt);
         Button btn = findViewById(R.id.btn);
         TextView t =findViewById(R.id.edt1);
+        TextView te =findViewById(R.id.edt2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String inputText = "Email:nghia.nhu.63cntt@ntu.edu.vn" ;
                 String txt="SĐT:0366541719";
+                String txt1="Link bài thi: https://github.com/nghiahuunguyen/63134775-AndroidProgramming/tree/main/thigk";
                 text.setText(inputText);
                 t.setText(txt);
+                te.setText(txt1);
 
             }
         });
