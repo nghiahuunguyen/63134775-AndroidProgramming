081ec49073f060dc4144f45ac156b3644d8b27b8
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2024-03-25 10:55:18 +0700

    .

--- thigk/Cau3_gioithieubanthan/app/src/main/java/ntu/nghia63134775/MainActivity.java
+++ thigk/Cau3_gioithieubanthan/app/src/main/java/ntu/nghia63134775/MainActivity.java
@@ -3,6 +3,9 @@ package ntu.nghia63134775;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = findViewById(R.id.edt);
+        Button btn = findViewById(R.id.btn);
+        TextView t =findViewById(R.id.edt1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = "Email:nghia.nhu.63cntt@ntu.edu.vn" ;
+                String txt="SĐT:0366541719";
+                text.setText(inputText);
+                t.setText(txt);
+
+            }
+        });
     }
 }
